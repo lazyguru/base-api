ce147987574e176c905db102acd58a91193b3084
Joe Constant <joe@joeconstant.com>
2015-10-30 22:30:42 -0600

    Change BaseSoapService class to no longer extend BaseService

--- src/BaseSoapService.php
+++ src/BaseSoapService.php
@@ -4,15 +4,32 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 
-abstract class BaseSoapService extends BaseService
+abstract class BaseSoapService
 {
     protected $wsdl        = null;
     private   $_soapClient = null;
-    private   $_auth       = [];
     private   $_options    = [];
 
     // Used for testing with a proxy (CharlesProxy) enabled
     private $_debug = false;
+
+    protected $_headers = [];
+    protected $uri = '';
+    protected $username = '';
+    protected $password = '';
+
+    /**
+     * @var LoggerInterface $output
+     */
+    protected $output;
+
+    /**
+     * @param LoggerInterface $log
+     * @param $username
+     * @param $password
+     * @param array $options
+     */
+    public abstract function __construct(LoggerInterface $log, $username, $password, $options = []);
 
     protected function processRequest($method, $params)
     {
